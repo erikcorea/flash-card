840473ecf349c4bd4f27eecddfbc92514b707966
erikcorea <erikcorrea444@gmail.com>
2020-10-08 20:24:49 -0500

    testing-one

---
+++ script.js
@@ -0,0 +1,51 @@
+const text = document.querySelector('textarea');
+const button = document.getElementById('button');
+const flipButton = document.getElementById('flip');
+const flipText = document.querySelector('h4');
+const studyButton = document.getElementById('study');
+const body = document.querySelector('body');
+let frontCard = [];
+let backCard = [];
+let please = {
+	front: frontCard,
+	back: backCard,
+};
+
+button.addEventListener('click', (event) => {
+	event.preventDefault();
+	if (text.value != '') {
+		if (flipText.innerText == 'front') {
+			frontCard.push(text.value);
+		} else if (flipText.innerText == 'back') {
+			backCard.push(text.value);
+		}
+	}
+});
+
+flipButton.addEventListener('click', (event) => {
+	event.preventDefault();
+	text.value = '';
+	if (flipText.innerText == 'front') {
+		flipText.innerText = 'back';
+	} else {
+		flipText.innerText = 'front';
+	}
+});
+
+function sad(){
+	for (let i = 0; i < frontCard.length; i++) {
+		const workPlease = document.createElement('textarea');
+		const maybe = document.createElement('textarea');
+		// workPlease.setAttribute('class', 'square');
+		workPlease.value = please.front[i];
+		maybe.value = please.back[i];
+		body.appendChild(workPlease);
+		body.appendChild(maybe);
+	}
+};
+
+studyButton.addEventListener('click', (event) => {
+	event.preventDefault();
+	sad();
+
+});
