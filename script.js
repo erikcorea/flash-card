b3bc68f19ce81004ed61247a39e3ff5665908abd
erikcorea <erikcorrea444@gmail.com>
2020-10-10 09:38:09 -0500

    setting-up-pages

--- script.js
+++ script.js
@@ -1,65 +1,98 @@
-const text = document.querySelector('textarea');
-const button = document.getElementById('save-button');
-const flipButton = document.getElementById('flip');
-const flipText = document.querySelector('h4');
-const studyButton = document.getElementById('study');
-const body = document.querySelector('body');
-// const hey = 'hey';
-let frontCard = [];
-let backCard = [];
-let please = {
-	front: frontCard,
-	back: backCard,
-};
-
-button.addEventListener('click', (event) => {
-	event.preventDefault();
-	if (text.value != '') {
-		if (flipText.innerText == 'front') {
-			frontCard.push(text.value);
-		} else if (flipText.innerText == 'back') {
-			backCard.push(text.value);
-		}
+class Cards {
+	constructor(front, back, setTitle) {
+		this.front = front;
+		this.back = back; 
+		this.setTitle = setTitle;
 	}
-});
 
-flipButton.addEventListener('click', (event) => {
-	event.preventDefault();
-	text.value = '';
-	if (flipText.innerText == 'front') {
-		flipText.innerText = 'back';
-	} else {
-		flipText.innerText = 'front';
-	}
-});
+	
+}
 
-function sad(){
-	for (let i = 0; i < frontCard.length; i++) {
-		const workPlease = document.createElement('textarea');
-		const maybe = document.createElement('textarea');
-		// workPlease.setAttribute('class', 'square');
-		workPlease.value = please.front[i];
-		maybe.value = please.back[i];
-		body.appendChild(workPlease);
-		body.appendChild(maybe);
-	}
-};
 
-studyButton.addEventListener('click', (event) => {
-	event.preventDefault();
-	sad();
 
-});
 
-//  class Card {
-// 		constructor(front, back) {
-// 			this.front = front;
-// 			this.back = back;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const text = document.querySelector('textarea');
+// const button = document.getElementById('save-button');
+// const flipButton = document.getElementById('flip');
+// const flipText = document.querySelector('h4');
+// const studyButton = document.getElementById('study');
+// const body = document.querySelector('body');
+// // const hey = 'hey';
+// let frontCard = [];
+// let backCard = [];
+// let please = {
+// 	front: frontCard,
+// 	back: backCard,
+// };
+
+// button.addEventListener('click', (event) => {
+// 	event.preventDefault();
+// 	if (text.value != '') {
+// 		if (flipText.innerText == 'front') {
+// 			frontCard.push(text.value);
+// 		} else if (flipText.innerText == 'back') {
+// 			backCard.push(text.value);
 // 		}
+// 	}
+// });
 
-// 		hey(){
-// 			return console.log(hey);
-// 		}
-//  }
+// flipButton.addEventListener('click', (event) => {
+// 	event.preventDefault();
+// 	text.value = '';
+// 	if (flipText.innerText == 'front') {
+// 		flipText.innerText = 'back';
+// 	} else {
+// 		flipText.innerText = 'front';
+// 	}
+// });
 
-//  console.loc(Card.hey());
+// function sad(){
+// 	for (let i = 0; i < frontCard.length; i++) {
+// 		const workPlease = document.createElement('textarea');
+// 		const maybe = document.createElement('textarea');
+// 		// workPlease.setAttribute('class', 'square');
+// 		workPlease.value = please.front[i];
+// 		maybe.value = please.back[i];
+// 		body.appendChild(workPlease);
+// 		body.appendChild(maybe);
+// 	}
+// };
+
+// studyButton.addEventListener('click', (event) => {
+// 	event.preventDefault();
+// 	sad();
+
+// });
+
+// //  class Card {
+// // 		constructor(front, back) {
+// // 			this.front = front;
+// // 			this.back = back;
+// // 		}
+
+// // 		hey(){
+// // 			return console.log(hey);
+// // 		}
+// //  }
+
+// //  console.loc(Card.hey());
