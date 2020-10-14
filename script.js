829399e705df1b2ed32f121745d6502fc6ceca9a
erikcorea <erikcorrea444@gmail.com>
2020-10-13 20:58:41 -0500

    adding-modal

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
+//Variables that are get from the html files
 const sideText = document.getElementById('flip-text');
-const flipButton = document.getElementById('flip');
 const textBox = document.getElementById('front-card');
-const nextButton = document.getElementById('next-button');
-const correctButton = document.getElementById('correct-button');
-const wrongButton = document.getElementById('wrong-button');
 const correctCounterText = document.getElementById('correct-number');
 const wrongCounterText = document.getElementById('wrong-number');
-const resetButton = document.getElementById('reset-button');
 const correctNumbCounter = document.getElementById('correct-number');
 const wrongNumbCounter = document.getElementById('wrong-number');
 
+//Getting the button that will change the side of the card
+const flipButton = document.getElementById('flip');
+//Gets the button that will change the to the next card
+const nextButton = document.getElementById('next-button');
+//Gets the button that will mark if you were correct
+const correctButton = document.getElementById('correct-button');
+//Gets the button that will mark if you were wrong
+const wrongButton = document.getElementById('wrong-button');
+//Gets the button that will reset the game
+const resetButton = document.getElementById('reset-button');
+
+//Array containing the english words that will be translated to spanish
 const english = ['Hello','Goodbye','Thank You','Please','Excuse Me','Im Sorry','Yes','No','food',
 'Car','Train','Bus','Nice to meet you','My name is ...','I dont understand','That was delicious',
 'You\'re welcome','What is your name?','How far is...?','Can you write that down?'];
 
+//Array containing the spanish words that were translated
 const spanish = ['Hola','Adiós','Gracias','Por favor','Perdóneme','Lo siento','Si','No','Comida',
 'Carro','El tren','Autobús','Gusto en conocerlo','Me llamo','No entiendo','Eso estuvo delicioso',
 'De nada','Cuál es su nombre','Que tan lejos esta','Puedes escribir eso'];
 
-
+//Counters that tells you what place in the array you are in
 let frontCounter = 0;
 let backCounter = 0;
+//Counter fo how many you have correct and wrong
 let correctCounterNumber = 0;
 let wrongCounterNumber = 0;
 
+//Object containing the arrays
 const info = {
 	front: english,
 	back: spanish,
 };
 
+//Setting the first  card when the page is loaded
 textBox.innerText = info.front[frontCounter];
 
+//When the button is flipped change the side text and 
+//The corresponding text side
 flipButton.addEventListener('click', (event) => {
 	event.preventDefault();
 	if (sideText.innerText == 'FRONT') {
@@ -42,6 +56,9 @@ flipButton.addEventListener('click', (event) => {
 	}
 });
 
+//When the next button is clicked it changes to the next card
+//This means that you either got it wrong or are
+//Skipping the word
 nextButton.addEventListener('click', (event) => {
 	event.preventDefault();
 	if (frontCounter < info.front.length - 1) {
@@ -58,6 +75,8 @@ nextButton.addEventListener('click', (event) => {
 	}
 });
 
+//When the correct button is clicked it adds it to the correct score
+//When you get all of them correct it resets the cards for you
 correctButton.addEventListener('click', (event) => {
 	event.preventDefault();
 	correctCounterNumber++;
@@ -69,34 +88,41 @@ correctButton.addEventListener('click', (event) => {
 
 	if(correctCounterNumber > 19){
 		location.reload();
-		frontCounter = 0;
-		backCounter = 0;
-		sideText.innerText = 'FRONT';
-		correctNumbCounter.innerText = '0';
-		wrongCounterText.innerText = '0';
-		textBox.innerText = info.front[frontCounter];
-
 	}
 });
 
+//When the wrong button is clicked it add it to the wrong score
 wrongButton.addEventListener('click', (event) => {
 	event.preventDefault();
 	wrongCounterNumber++;
 	wrongCounterText.innerText = `${wrongCounterNumber}`;
 });
 
-
+//Resets the cards and score
 resetButton.addEventListener('click', (event) => {
 	event.preventDefault();
-	frontCounter = 0;
-	backCounter = 0;
-	sideText.innerText = 'FRONT';
-	correctNumbCounter.innerText = '0';
-	wrongCounterText.innerText = '0';
-	textBox.innerText = info.front[frontCounter];
-})
+	location.reload();
+});
 
 
+const openButton = document.getElementById('openModal');
+
+const modal = document.getElementById('modal');
+
+const closeButton = document.getElementById('close');
+
+const openModal = () => {
+	modal.style.display = 'block';
+};
+
+const closeModal = () => {
+	modal.style.display = 'none';
+};
+
+openButton.addEventListener('click', openModal);
+closeButton.addEventListener('click', closeModal);
+
+//My failed attempts at strech goals
 // const sideText = document.getElementById('flip-text');
 // const flipButton = document.getElementById('flip');
 // const textBox = document.getElementById('front-card');
